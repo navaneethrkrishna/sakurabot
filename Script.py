c52d6beb5185b40cd6dbbf888b6d21ac433e15c7
navaneethrkrishna <95025235+navaneethrkrishna@users.noreply.github.com>
2022-01-14 15:20:22 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -33,7 +33,7 @@ class script(object):
 𝖬𝖺𝖽𝖾 𝖻𝗒 
 """
     ABOUT_TXT = """
-○ 𝖬𝗒 𝖭𝖺𝗆𝖾 : 𝖲𝖺𝗄𝗎𝗋𝖺
+○ 𝖬𝗒 𝖭𝖺𝗆𝖾 : 𝖲�pider man 
 ○ 𝖢𝗋𝖾𝖺𝗍𝗈𝗋 : <a href='https://t.me/DJ_Alok_Ser_TG'>A</a>
 ○ 𝖫𝖺𝗇𝗀𝗎𝖺𝗀𝖾 : 𝖯𝗒𝗍𝗁𝗈𝗇 𝟥 
 ○ 𝖫𝗂𝖻𝗋𝖺𝗋𝗒 : 𝖯𝗒𝗋𝗈𝗀𝗋𝖺𝗆 𝖺𝗌𝗒𝗇𝖼𝗂𝗈 𝟢.𝟣𝟩.𝟣 
